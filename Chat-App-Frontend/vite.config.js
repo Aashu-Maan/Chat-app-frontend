b845528b09d188e45b9ab6f7c41e67f79d74aaaf
Aashu-Maan <aashumaan23820@gmail.com>
2025-09-17 21:08:20 +0530

    files updated

--- Chat-App-Frontend/vite.config.js
+++ Chat-App-Frontend/vite.config.js
@@ -3,8 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 ensures correct asset paths
-  build: {
-    outDir: 'dist', // Vercel expects this
-  },
 })
