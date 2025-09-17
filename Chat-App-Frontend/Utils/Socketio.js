f4709a51987c55357dafb3053618f13a0bcff6a3
Aashu-Maan <aashumaan23820@gmail.com>
2025-09-17 19:17:37 +0530

    sockets updated

--- Chat-App-Frontend/Utils/Socketio.js
+++ Chat-App-Frontend/Utils/Socketio.js
@@ -4,10 +4,21 @@ export const createSocketConnection = () => {
   return io("http://localhost:9000");
 }
 */
+/*
 import { io } from "socket.io-client";
 
 export const createSocketConnection = () => {
-  return io("http://localhost:5491", {
+  return io("https://chat-app-backend-2-ab1j.onrender.com", {
     transports: ["websocket"], // force websocket (avoid long polling spam)
+  });
+};
+*/
+
+import { io } from "socket.io-client";
+
+export const createSocketConnection = () => {
+  return io("https://chat-app-backend-2-ab1j.onrender.com", {
+    transports: ["websocket"],
+    withCredentials: true, // ✅ optional but recommended
   });
 };
